a1aa218a8dd980cc1cdfbfe140b7889954dc673f
Savannah Mann <sv.bluecow@gmail.com>
2018-08-28 15:27:57 -0500

    TEA update
    https://i.kym-cdn.com/entries/icons/original/000/015/878/thatsnoneofmy.jpg

---
+++ UnityPort/Protagonist/Assets/Scripts/Puzzle/Spells/TeaSpell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TeaSpell : ProjectileSpell
+{
+    public GameObject hitEffect;
+    public GameObject smokeEffect;
+
+    ParticlesAttached ps;
+    protected override void Initialize()
+    {
+        Vector2 pos = ScreenResolution.MapViewToWorldPoint(new Vector2(0.4f, 0.2f));
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+        // create smoke obj
+        ps = Instantiate(smokeEffect, transform.position, transform.rotation).GetComponent<ParticlesAttached>();
+        ps.Initialize(this.gameObject);
+    }
+
+    protected override void StartMoving()
+    {
+        ps.Play();
+    }
+
+    protected override void FinishMoving()
+    {
+        Instantiate(hitEffect, transform.position, transform.rotation);
+        ps.Destroy();
+        Destroy(gameObject);
+    }
+}
